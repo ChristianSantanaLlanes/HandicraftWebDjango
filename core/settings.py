41510b674fc3ed14b1575b128d97ddfdf4b6221e
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2022-08-22 21:15:19 -0400

    nuevo

--- core/settings.py
+++ core/settings.py
@@ -260,8 +260,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = "static"
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = "static"
+# STATICFILES_DIRS = [STATIC_DIR]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 WHITENOISE_AUTOREFRESH = True
 
